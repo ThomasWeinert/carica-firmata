8aaefa78a36556a08c705fc1745c98741b59d5ff
Thomas Weinert <thomas@weinert.info>
2013-06-12 19:57:14 +0200

    Added PHP+JS version of the firmata-test gui

---
+++ example/firmata-test/files/firmata.js
@@ -0,0 +1,250 @@
+(function($) {
+  
+  var pin = {
+
+    board : null,
+    element : null,
+    number : 0,
+    mode : 'output',
+    digital : 'yes',
+    analog : 0,
+    blockUpdates : false,
+    isChanged : false,
+    
+    template : 
+      '<div class="span4">' +
+        '<form id="pin{X}" class="pin-control well clearfix">' +
+          '<h3 class="form-heading">Pin {X}</h3>' +
+          '<div class="control-group pin-modes">' +
+            '<div class="controls btn switch switch-five">' +
+              '<input id="pin{X}_mode_output" name="pin2_mode" value="output" type="radio">' +
+              '<label for="pin{X}_mode_output">output</label>' +
+              '<input id="pin{X}_mode_input" name="pin2_mode" value="input" type="radio">' +
+              '<label for="pin{X}_mode_input">input</label>' +
+              '<input id="pin{X}_mode_pwm" name="pin2_mode" value="pwm" type="radio">' +
+              '<label for="pin{X}_mode_pwm">pwm</label>' +
+              '<input id="pin{X}_mode_analog" name="pin2_mode" value="analog" type="radio">' +
+              '<label for="pin{X}_mode_analog">analog</label>' +
+              '<input id="pin{X}_mode_servo" name="pin2_mode" value="servo" type="radio">' +
+              '<label for="pin{X}_mode_servo">servo</label>' +
+              '<span class="slide-button btn btn-success"></span>' +
+            '</div>' +
+          '</div>' +
+          '<div class="control-group pin-mode pin-mode-output">' +
+            '<div class="controls btn switch switch-two">' +
+              '<input id="pin{X}_digital_yes" name="pin{X}_digital" value="yes" type="radio">' +
+              '<label for="pin{X}_digital_yes">on</label>' +
+              '<input id="pin{X}_digital_no" name="pin{X}_digital" value="no" type="radio">' +
+              '<label for="pin{X}_digital_no">off</label>' +
+              '<span class="slide-button btn btn-success"></span>' +
+            '</div>' +
+          '</div>' +
+          '<div class="control-group pin-mode pin-mode-input" style="display: none;">' +
+            '<div class="controls btn switch switch-two disabled">' +
+              '<input id="pin{X}_digital_yes" name="pin{X}_digital" value="yes" type="radio" disabled>' +
+              '<label for="pin{X}_digital_yes" class="disabled">on</label>' +
+              '<input id="pin{X}_digital_no" name="pin{X}_digital" value="no" type="radio" disabled>' +
+              '<label for="pin{X}_digital_no" class="disabled">off</label>' +
+              '<span class="slide-button btn btn-success"></span>' +
+            '</div>' +
+          '</div>' +
+          '<div class="pin-mode pin-mode-pwm" style="display: none;">' +
+            '<input type="number" id="pin{X}_pwm" min="0" max="255" step="1">' +
+          '</div>' +
+          '<div class="pin-mode pin-mode-analog" style="display: none;">' +
+            '<div class="progress progress-success progress-striped" id="pin{X}_analog">' +
+              '<div class="bar" style="width: 20%;"></div>' +
+            '</div>' +
+          '</div>' +
+          '<div class="pin-mode pin-mode-servo" style="display: none;">' +
+            '<input type="number" id="pin{X}_servo" min="0" max="255" step="1">' +
+          '</div>' +
+        '</form>' +
+      '</div>',
+      
+      
+    setUp : function(board, pinData) {
+      this.board = board;
+      this.number = pinData.attr('number');
+      board.container.append(this.template.replace(/\{X\}/g, this.number));
+      this.element = board.container.find('#pin' + this.number);
+      this.element.find('.pin-modes input').each(
+        function () {
+          var input = $(this);
+          var mode = input.val();
+          if (!(pinData.is('[supports~=' + mode + ']'))) {
+            input.prop('disabled', true);
+            input.parent().find('label[for=' + input.attr('id') +']').addClass('disabled');
+          }
+        }  
+      );
+      this.element.find('.pin-mode-input label').click(
+        function(event) {
+          event.stopPropagation();
+          return false;
+        }
+      );
+      this.element.find('input[type="radio"]').click(
+          $.proxy(this.send, this)
+        );
+      this.element.find('input[type="number"]').change(
+          $.proxy(this.send, this)
+        );
+      this.update(pinData);
+      return this;
+    },
+  
+    update : function(pinData) {
+      if (this.blockUpdates) {
+        return;
+      }
+      if (typeof pinData != 'undefined') {
+        this.mode = pinData.attr('mode');
+        this.digital = pinData.attr('digital');
+        if (typeof this.digital == 'undefined') {
+          this.digital = 'no';
+        }
+        this.analog = pinData.attr('analog');
+        if (typeof this.analog == 'undefined') {
+          this.analog = 0;
+        }
+      }
+      var selected = '.pin-mode-' + this.mode;
+      this.element.find('.pin-modes input[value="' + this.mode + '"]').prop('checked', true);
+      this.element.find('.pin-mode').not(selected).hide();
+      this.element.find(selected).show();
+      switch (this.mode) {
+      case 'output' :
+        this.element.find('.pin-mode-output input[value="' + this.digital + '"]').prop('checked', true);
+        break;
+      case 'input' :
+        if (!this.element.find('.pin-mode-input input[value="' + this.digital + '"]').is(':checked')) {
+          this.element.find('.pin-mode-input input[value="' + this.digital + '"]').prop('checked', true);
+        }
+        break;
+      case 'pwm' :
+        this.element.find('.pin-mode-pwm input:not(:focus)').val(this.analog);
+        break;
+      case 'analog' :
+        this.element.find('.pin-mode-analog .progress .bar').css(
+          'width',
+          Math.round(
+            parseInt(this.analog) * 100 / 1023
+          ).toString() + '%'
+        );
+        break;
+      case 'servo' :
+        this.element.find('.pin-mode-servo input:not(:focus)').val(this.analog);
+        break;
+      }
+    },
+    
+    send : function () {
+      if (this.blockUpdates) {
+        this.isChanged = true;
+      }
+      this.blockUpdates = true;
+      this.mode = this.element.find('.pin-modes input:checked').attr('value');
+      var url = this.board.options.url + '/' + this.number + '?mode=' + this.mode;
+      switch (this.mode) {
+      case 'output' :
+        this.digital = this.element.find('.pin-mode-output input:checked').attr('value');
+        url += '&digital=' + this.digital;
+        break;
+      case 'input' :
+      case 'analog' :
+        break;
+      case 'pwm' :
+        this.analog = this.element.find('.pin-mode-pwm input').prop('value');
+        url += '&analog=' + this.analog;
+        break;
+      case 'servo' :
+        this.analog = this.element.find('.pin-mode-servo input').prop('value');
+        url += '&analog=' + this.analog;
+        break;
+      }
+      this.update();
+      var that = this;
+      $.get(url)
+       .always(
+         function () {
+           that.blockUpdates = false;
+         }
+       )
+       .done(
+         function (response) {
+           that.blockUpdates = false;
+           $(response).find('board pin').each(
+             function() {
+               var key = 'pin' + $(this).attr('number');
+               that.board.pins[key].update($(this));
+             }  
+           );
+           if (that.isChanged) {
+             that.isChanged = false;
+             that.send();
+           }
+         }
+       );
+    }      
+  };
+  
+  var board = {
+    
+    container : null,
+    pins : {},
+    
+    options : {
+      url : '../pins'
+    },
+    
+    setUp : function(container, options) {
+      this.container = container;
+      this.options = $.extend(this.options, options);
+      var that = this;
+      $.get(this.options.url)
+       .done(
+         function (response) {
+           $(response).find('board pin').each(
+             function() {
+               var key = 'pin' + $(this).attr('number');
+               that.pins[key] = $.extend(true, {}, pin).setUp(that, $(this));
+             }  
+           );
+           that.schedule();
+         }
+       );
+      return this;
+    },
+    
+    schedule : function() {
+      var that = this;
+      window.setTimeout(
+        function() {
+          $.get(that.options.url)
+           .done(
+             function (response) {
+               $(response).find('board pin').each(
+                 function() {
+                   var key = 'pin' + $(this).attr('number');
+                   that.pins[key].update($(this));
+                 }  
+               );
+               that.schedule();
+             }
+           );
+        },
+        1000
+      );
+    }
+    
+  };
+  
+  $.fn.firmata = function(options) {
+    var container = $(this).eq(0);
+    if (container.length > 0) {
+      board.setUp(container, options);
+    };
+  };
+  
+})(jQuery);
---
+++ example/firmata-test/firmata.php
@@ -0,0 +1,39 @@
+<?php
+$board = require('../bootstrap.php');
+
+use Carica\Io;
+use Carica\Firmata;
+use Carica\Io\Network\Http;
+
+$route = new Carica\Io\Network\Http\Route();
+$route->match('/pins', new Firmata\Rest\Pins($board));
+$route->match('/pins/{pin}', new Firmata\Rest\Pin($board));
+$route->startsWith('/files', new Http\Route\File(__DIR__));
+$route->match(
+  '/',
+  function ($request) {
+    $response = $request->createResponse();
+    $response->content = new Http\Response\Content\File(
+      __DIR__.'/index.html', 'text/html', 'utf-8'
+    );
+    return $response;
+  }
+);
+
+$board
+  ->activate()
+  ->done(
+    function () use ($board, $route) {
+      $board->queryAllPinStates();
+      $server = new Carica\Io\Network\Http\Server($route);
+      $server->listen(8080);
+    }
+  )
+  ->fail(
+    function ($error) {
+      echo $error."\n";
+    }
+  );
+
+
+Carica\Io\Event\Loop\Factory::run();
