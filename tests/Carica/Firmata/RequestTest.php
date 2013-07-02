ed9387df72110f7ff1b375a6eec8d2f2f5b85fa0
Thomas Weinert <thomas@weinert.info>
2013-07-02 22:48:12 +0200

    Tested: Carica/Firmata/RequestTest.php

---
+++ tests/Carica/Firmata/RequestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Carica\Firmata {
+
+  include_once(__DIR__.'/Bootstrap.php');
+
+  class RequestTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @covers Carica\Firmata\Request
+     */
+    public function testConstrutor() {
+      $board = $this
+        ->getMockBuilder('Carica\Firmata\Board')
+        ->disableOriginalConstructor()
+        ->getMock();
+      $request = new Request_TestProxy($board);
+      $this->assertSame($board, $request->board());
+    }
+
+    /**
+     * @covers Carica\Firmata\Request
+     */
+    public function testEncodeBytes() {
+      $this->assertSame(
+        "\x53\x00\x61\x00\x6D\x00\x70\x00\x6C\x00\x65\x00",
+        Request::encodeBytes('Sample')
+      );
+    }
+  }
+
+  class Request_TestProxy extends Request {
+
+    public function send() {
+
+    }
+
+  }
+}
