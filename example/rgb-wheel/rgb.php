4105bf145c2765bb3bd36737d49b0f8a2bca3aa5
Thomas Weinert <thomas@weinert.info>
2014-01-27 21:50:22 +0100

    Add local jquery for rgb-wheel example, use pins available on arduino uno/nano

--- example/rgb-wheel/rgb.php
+++ example/rgb-wheel/rgb.php
@@ -11,15 +11,15 @@ $route->match(
   function (Http\Request $request) use ($board) {
     if (isset($request->query['r'])) {
       $red = (int)$request->query['r'];
-      $board->pins[20]->value = ($red > 0 && $red < 256) ? $red : 0;
+      $board->pins[3]->value = ($red > 0 && $red < 256) ? $red : 0;
     }
     if (isset($request->query['g'])) {
       $green = (int)$request->query['g'];
-      $board->pins[21]->value = ($green > 0 && $green < 256) ? $green : 0;
+      $board->pins[5]->value = ($green > 0 && $green < 256) ? $green : 0;
     }
     if (isset($request->query['b'])) {
       $blue = (int)$request->query['b'];
-      $board->pins[22]->value = ($blue > 0 && $blue < 256) ? $blue : 0;
+      $board->pins[6]->value = ($blue > 0 && $blue < 256) ? $blue : 0;
     }
     $response = $request->createResponse();
     $response->content = new Http\Response\Content\String(
@@ -37,9 +37,9 @@ $board
   ->activate()
   ->done(
     function () use ($board, $route) {
-      $board->pins[20]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
-      $board->pins[21]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
-      $board->pins[22]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
+      $board->pins[3]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
+      $board->pins[5]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
+      $board->pins[6]->mode = Carica\Firmata\Board::PIN_MODE_PWM;
       $server = new Carica\Io\Network\Http\Server($route);
       $server->listen(8080);
     }
