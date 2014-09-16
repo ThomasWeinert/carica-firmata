f08340f93949a47136498894b26f0c3691532f57
Thomas Weinert <thomas@weinert.info>
2014-09-16 22:23:46 +0200

    Added documentation

--- src/Version.php
+++ src/Version.php
@@ -2,23 +2,50 @@
 
 namespace Carica\Firmata {
 
+  /**
+   * Immutable data object for version informations
+   */
   class Version {
 
+    /**
+     * @var string
+     */
     private $_text = '';
+
+    /**
+     * @var int
+     */
     private $_major = 0;
+
+    /**
+     * @var int
+     */
     private $_minor = 0;
 
+    /**
+     * @param string $major
+     * @param string $minor
+     * @param string $text
+     */
     public function __construct($major, $minor, $text = '') {
       $this->_major = (int)$major;
       $this->_minor = (int)$minor;
       $this->_text = trim($text);
     }
 
+    /**
+     * @return string
+     */
     public function __toString() {
       $result = empty($this->_text) ? '' : $this->_text.' ';
       return $result.$this->_major.'.'.$this->_minor;
     }
 
+    /**
+     * @param string $name
+     * @return mixed
+     * @throws \LogicException
+     */
     public function __get($name) {
       switch ($name) {
       case 'text' :
@@ -29,6 +56,13 @@ namespace Carica\Firmata {
       throw new \LogicException(sprintf('Unknown property %s::$%s', get_class($this), $name));
     }
 
+    /**
+     * Block changes to the properties
+     *
+     * @param string $name
+     * @param mixed $value
+     * @throws \LogicException
+     */
     public function __set($name, $value) {
       throw new \LogicException(sprintf('Object %s can not be changed.', get_class($this)));
     }
