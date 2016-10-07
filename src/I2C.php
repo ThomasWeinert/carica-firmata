c2470af4ce5737b549401dba69d726d87eda0320
Thomas Weinert <thomas@weinert.info>
2016-10-07 11:58:11 +0200

    Removed deviceAddress from i2c interface, added as constructor argument.

--- src/I2C.php
+++ src/I2C.php
@@ -3,12 +3,12 @@ namespace Carica\Firmata {
 
   use Carica\Io\ByteArray;
   use Carica\Io\Deferred;
-  use Carica\Io\Event;
   use Carica\Io\Device;
 
   class I2C implements Device\I2C {
 
-    use Event\Emitter\Aggregation;
+    use /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+      \Carica\Io\Event\Emitter\Aggregation;
 
     const REQUEST = 0x76;
     const REPLY = 0x77;
@@ -34,7 +34,12 @@ namespace Carica\Firmata {
      */
     private $_isInitialized = FALSE;
 
-    public function __construct(Board $board) {
+    /**
+     * @var int $_deviceAddress
+     */
+    private $_deviceAddress;
+
+    public function __construct(Board $board, $deviceAddress) {
       $this->_board = $board;
       $this->_board->events()->on(
         'response',
@@ -45,6 +50,10 @@ namespace Carica\Firmata {
           }
         }
       );
+      $this->_deviceAddress = (int)$deviceAddress;
+      if ($this->_deviceAddress <= 0 || $this->_deviceAddress > 127) {
+        throw new \OutOfRangeException('Invalid I2C address.');
+      }
     }
 
     /**
@@ -59,14 +68,13 @@ namespace Carica\Firmata {
      * Emit the debug event with the address as hex string and data bytes in binary representation.
      * 
      * @param $method
-     * @param $slaveAddress
      * @param $data
      */
-    private function emitDebug($method, $slaveAddress, $data) {
+    private function emitDebug($method, $data) {
       $this->emitEvent(
         'debug', 
         $method, 
-        '0x'.str_pad(dechex($slaveAddress), 2, '0', STR_PAD_LEFT), 
+        '0x'.str_pad(dechex($this->_deviceAddress), 2, '0', STR_PAD_LEFT),
         ByteArray::createFromArray($data)->asBitString()
       );
     }
@@ -100,16 +108,15 @@ namespace Carica\Firmata {
     /**
      * Write some data to an i2c device
      *
-     * @param integer $slaveAddress
-     * @param string $data
+     * @param array $data
      */
-    public function write($slaveAddress, $data) {
+    public function write(array $data) {
       if ($this->_debug) {
-        $this->emitDebug(__FUNCTION__, $slaveAddress, $data);
+        $this->emitDebug(__FUNCTION__, $data);
         return;
       }
       $this->ensureConfiguration();
-      $request = new I2C\Request($this->_board, $slaveAddress, self::MODE_WRITE, $data);
+      $request = new I2C\Request($this->_board, $this->_deviceAddress, self::MODE_WRITE, $data);
       $request->send();
     }
 
@@ -117,15 +124,14 @@ namespace Carica\Firmata {
      * Request data from an i2c device and trigger callback if the
      * data is sent.
      *
-     * @param integer $slaveAddress
      * @param integer $byteCount
      * @return Deferred
      */
-    public function read($slaveAddress, $byteCount) {
+    public function read($byteCount) {
       $this->ensureConfiguration();
       $defer = new Deferred();
       $this->events()->once(
-        'reply-'.$slaveAddress,
+        'reply-'.$this->_deviceAddress,
         function ($bytes) use ($defer, $byteCount) {
           if (count($bytes) == $byteCount) {
             $defer->resolve($bytes);
@@ -135,43 +141,40 @@ namespace Carica\Firmata {
         }
       );
       $request = new I2C\Request(
-        $this->_board, $slaveAddress, self::MODE_READ, $byteCount
+        $this->_board, $this->_deviceAddress, self::MODE_READ, $byteCount
       );
       $request->send();
       return $defer;
     }
 
     /**
-     * @param int $slaveAddress
      * @param int $byteCount
      * @param callable $listener
      */
-    public function startReading($slaveAddress, $byteCount, callable $listener) {
+    public function startReading($byteCount, callable $listener) {
+      $deviceAddress = $this->_deviceAddress;
       $this->ensureConfiguration();
-      $this->stopReading($slaveAddress);
+      $this->stopReading();
       $this->events()->on(
-        'reply-'.$slaveAddress,
-        function ($bytes) use ($slaveAddress, $listener, $byteCount) {
+        'reply-'.$deviceAddress,
+        function ($bytes) use ($deviceAddress, $listener, $byteCount) {
           if (count($bytes) == $byteCount) {
             $listener($bytes);
           } else {
-            $this->stopReading($slaveAddress);
+            $this->stopReading();
           }
         }
       );
       $request = new I2C\Request(
-        $this->_board, $slaveAddress, self::MODE_CONTINOUS_READ, $byteCount
+        $this->_board, $deviceAddress, self::MODE_CONTINOUS_READ, $byteCount
       );
       $request->send();
     }
 
-    /**
-     * @param int $slaveAddress
-     */
-    public function stopReading($slaveAddress) {
-      $this->events()->removeAllListeners('reply-'.$slaveAddress);
+    public function stopReading() {
+      $this->events()->removeAllListeners('reply-'.$this->_deviceAddress);
       $request = new I2C\Request(
-        $this->_board, $slaveAddress, self::MODE_STOP_READING
+        $this->_board, $this->_deviceAddress, self::MODE_STOP_READING
       );
       $request->send();
     }
